73e66e2f9359e926ba309e570d73d2cf351f93b3
Spiros Tzavellas <spiros@tzavellas.com>
2010-05-23 03:40:16 +0300

    Documented @Controller.

--- src/main/java/com/coeusweb/spring/mvc/Controller.java
+++ src/main/java/com/coeusweb/spring/mvc/Controller.java
@@ -9,6 +9,17 @@ package com.coeusweb.spring.mvc;
 import java.lang.annotation.*;
 import org.springframework.context.annotation.Scope;
 
+/**
+ * A Spring meta-annotation that can be used for annotating Coeus {@code Controller} classes.
+ *
+ * <p>The usage of this annotation is not necessary for using Coeus with Spring but it is
+ * recommended.</p>
+ *
+ * <p>This annotation is annotated with the Spring's {@literal @Controller} annotation so controllers
+ * annotated with this annotation can be automatically discovered in Spring's classpath scanning. Also
+ * this annotation is annotated with the {@literal @Scope("prototype")} annotation so the controllers
+ * will also get configured in the prototype scope.</p>
+ */
 @org.springframework.stereotype.Controller
 @Scope("prototype")
 @Retention(RetentionPolicy.RUNTIME)
